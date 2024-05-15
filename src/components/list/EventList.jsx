2f58e89204e801af1cdefddbfc9947b21efd3a55
Dee Watson <dlarkinwatson@gmail.com>
2024-05-15 00:01:36 -0700

    feat: Imports data sorted by day and start time

--- src/components/list/EventList.jsx
+++ src/components/list/EventList.jsx
@@ -1,17 +1,32 @@
 import React from "react";
-import events from "../../../assets/data/sample.json";
+import events from "../../../assets/data/events.json";
 import { EventDetails } from "../details/EventDetails";
 
 export const EventList = () => {
-  let eventNum = 0;
-  const eventList = events["SOAK 2024"].map((event) => {
-    eventNum++;
-    return (
-      <section key={eventNum}>
+  const eventList = events
+    .sort((a, b) => {
+      const days = ["Thursday", "Friday", "Saturday", "Sunday"];
+      return (
+        days.indexOf(a.day) - days.indexOf(b.day) ||
+        parseTimestamp(a.when) - parseTimestamp(b.when)
+      );
+    })
+    .map((event, index) => (
+      <section key={index}>
         <EventDetails {...event} />
       </section>
-    );
-  });
+    ));
 
   return <div className="event-list">{eventList}</div>;
+};
+
+const parseTimestamp = (str) => {
+  const [start, period] = (str ?? "").split("-")[0].split(" ");
+  const [startHour, startMinute] = start.split(":");
+
+  return (
+    (parseInt(startHour) % 12) +
+    (period === "PM" ? 12 : 0) * 60 +
+    parseInt(startMinute)
+  );
 };
