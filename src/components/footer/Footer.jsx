61f90615ffef54b141795850047e5c386f85b637
Dee Larkin <deelarkin@proton.me>
2024-05-16 14:30:08 -0700

    feat: Creates footer tabs and logic to filter events by day

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Tab } from "./Tab";
+
+const TABS = {
+  DAILY: "#333333",
+  THU: "#02597F",
+  FRI: "#1FB2A3",
+  SAT: "#FAAF40",
+  SUN: "#D9533D",
+};
+
+export const Footer = () => {
+  return (
+    <div className="footer-tab-wrapper">
+      {Object.keys(TABS).map((day, index) => (
+        <Tab day={day} color={TABS[day]} key={index} />
+      ))}
+    </div>
+  );
+};
