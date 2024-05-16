de81a512a4ba1d40f8e56835a8e5c753f1376494
NickDayFSD <Nick.Day.FSD@gmail.com>
2024-05-15 22:54:52 -0700

    cleanup

--- src/components/details/EventTile.jsx
+++ src/components/details/EventTile.jsx
@@ -10,22 +10,12 @@ export const EventTile = (event) => {
     Sunday: "#F9e0a6",
   };
 
-  const toDo = ["calculate day/night/all day", "some events have no area"];
+  // const toDo = ["calculate day/night/all day", "some events have no area"];
   return (
     <div
       className="event-details"
       style={{ "--date-color": [date[event.day]] }}
     >
-      {/* <div className="event-details-title">
-        <div>
-          {event.when} {event.day}
-        </div>
-        <div>{event.where}</div>
-      </div>
-      <div className="event-details-pill-group">
-        <div>{event.area}</div>
-        <div>{event.eventType.toLowerCase()}</div>
-      </div> */}
       <EventTileHeader event={event} />
       <div className="event-details-description">{event.what}</div>
       <EventTileFooter event={event} />
