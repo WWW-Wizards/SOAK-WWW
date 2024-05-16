13452d6f1b7ff9b9b92f5499b4bdcdd94099f124
NickDayFSD <Nick.Day.FSD@gmail.com>
2024-05-16 16:39:18 -0700

    Adding svg components and icon management

---
+++ src/components/details/CatIcon.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import CatArt from "./icons/CatArt";
+import CatClass from "./icons/CatClass";
+import CatEntertainment from "./icons/CatEntertainment";
+import CatFoodBev from "./icons/CatFoodBev";
+import CatMisc from "./icons/CatMisc";
+
+function CatIcon({ event }) {
+  // Figure out which icons to display based on category
+  return (
+    <div>
+      <CatMisc />
+    </div>
+  );
+}
+
+export default CatIcon;
--- src/components/details/EventTileFooter.jsx
+++ src/components/details/EventTileFooter.jsx
@@ -4,8 +4,7 @@ function EventTileFooter({ event }) {
   //console.log(`Event tile footer event: ${event}`);
   return (
     <section className="event-details-footer">
-      <div className="event-details-footer-left">category icons</div>
-      <div className="event-details-footer-center">X</div>
+      <div className="event-details-footer-left">{event.where}</div>
       {event.area !== null ? (
         <div className="event-details-footer-right">{event.area}</div>
       ) : (
