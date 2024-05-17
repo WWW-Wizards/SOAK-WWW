859337c2cea52d7d7251c29d9d79535543158765
Dee Larkin <deelarkin@proton.me>
2024-05-16 22:34:54 -0700

    chore: Updates imports and runs formatter

--- src/state/StateProvider.js
+++ src/state/StateProvider.js
@@ -1,15 +1,15 @@
-import React, { useState, createContext, useContext, useEffect } from 'react';
+import React, { useState, createContext, useContext, useEffect } from "react";
 import data from "../../assets/data/events.json";
 
 export const UserContext = createContext();
 
-const FILTERS = { 
-  FAVORITES: 'FAVORITES', 
-  ZONE: 'ZONE', 
-  CAMP: 'CAMP',
-  TIME: 'TIME', 
-  CATEGORY: 'CATEGORY'
-}
+const FILTERS = {
+  FAVORITES: "FAVORITES",
+  ZONE: "ZONE",
+  CAMP: "CAMP",
+  TIME: "TIME",
+  CATEGORY: "CATEGORY",
+};
 
 const DAYS = {
   DAILY: "DAILY",
@@ -34,51 +34,52 @@ export function StateProvider({ children }) {
   const [events, setEvents] = useState(data);
   const [favorites, setFavorites] = useState(() => {
     // Check local storage if favorites exist, otherwise initialize an empty array
-    const cachedFavorites = localStorage.getItem('favorites');
+    const cachedFavorites = localStorage.getItem("favorites");
     return cachedFavorites ? JSON.parse(cachedFavorites) : [];
   });
   const [activeTab, setActiveTab] = useState(DAYS.DAILY);
   const [date, setDate] = useState(DAY_HEADINGS.DAILY);
+  const [query, setQuery] = useState("");
 
   // Handles toggling the filter view between favorited events and all events
-  const handleFilterFavorites = (e) => { 
-    // Prevents toggling the menu accordion when clicking on favorites 
-    e.stopPropagation(); 
+  const handleFilterFavorites = (e) => {
+    // Prevents toggling the menu accordion when clicking on favorites
+    e.stopPropagation();
 
     // If the favorites filter is already active, unset it (toggle)
     const newFilter = filter === FILTERS.FAVORITES ? "" : FILTERS.FAVORITES;
     setFilter(newFilter);
   };
-  
-  // Handles toggling an individual event into or out of the favorites list  
-  const handleToggleFavorited = (id) => { 
-    setFavorites(prev => {
-      const prevIndex = prev.findIndex(favorite => favorite.id === id);
-      
+
+  // Handles toggling an individual event into or out of the favorites list
+  const handleToggleFavorited = (id) => {
+    setFavorites((prev) => {
+      const prevIndex = prev.findIndex((favorite) => favorite.id === id);
+
       // Check if the event is already favorited
       // If it is, unfavorite it
-      // If it isn't, use its id to look up the corresponding event data and add it to the favorites list 
-      const newFavorites = prevIndex !== -1 
-      ? prev.filter(favorite => favorite.id !== id) 
-        : [...prev, events.find(event => event.id === id)];
+      // If it isn't, use its id to look up the corresponding event data and add it to the favorites list
+      const newFavorites =
+        prevIndex !== -1
+          ? prev.filter((favorite) => favorite.id !== id)
+          : [...prev, events.find((event) => event.id === id)];
 
       // Set favorite into local storage
-      localStorage.setItem('favorites', JSON.stringify(newFavorites));
-      
+      localStorage.setItem("favorites", JSON.stringify(newFavorites));
+
       return newFavorites;
     });
-
   };
 
   // Returns a string to be used as a classname for conditional styling
-  const handleFavoriteDisplay = (id) => { 
-    const isFavorited = favorites.some(favorite => favorite.id === id);
+  const handleFavoriteDisplay = (id) => {
+    const isFavorited = favorites.some((favorite) => favorite.id === id);
     return isFavorited ? "favorited" : "";
-  }
-  
-  const handleTabClick = (day) => { 
+  };
+
+  const handleTabClick = (day) => {
     setActiveTab(day);
-  }
+  };
 
   const handleSearch = (query) => {
     setQuery(query);
@@ -86,7 +87,7 @@ export function StateProvider({ children }) {
 
   useEffect(() => {
     // Update local storage whenever favorites state changes
-    localStorage.setItem('favorites', JSON.stringify(favorites));
+    localStorage.setItem("favorites", JSON.stringify(favorites));
 
     setDate(DAY_HEADINGS[activeTab]);
 
@@ -121,8 +122,8 @@ export function StateProvider({ children }) {
         filter,
         setFilter,
         menu,
-        setMenu, 
-        events, 
+        setMenu,
+        events,
         setEvents,
         handleFilterFavorites,
         handleToggleFavorited,
@@ -145,20 +146,34 @@ export function StateProvider({ children }) {
 export const useLoading = () => {
   const { loading, setLoading } = useContext(UserContext);
   return { loading, setLoading };
-}
+};
 
 export const useFilter = () => {
-  const { filter, setFilter, FILTERS, handleFavoriteDisplay, handleFilterFavorites, handleToggleFavorited } = useContext(UserContext);
-  return { filter, setFilter, FILTERS, handleFavoriteDisplay, handleFilterFavorites, handleToggleFavorited };
-}
+  const {
+    filter,
+    setFilter,
+    FILTERS,
+    handleFavoriteDisplay,
+    handleFilterFavorites,
+    handleToggleFavorited,
+  } = useContext(UserContext);
+  return {
+    filter,
+    setFilter,
+    FILTERS,
+    handleFavoriteDisplay,
+    handleFilterFavorites,
+    handleToggleFavorited,
+  };
+};
 
 export const useMenu = () => {
   const { menu, setMenu } = useContext(UserContext);
   return { menu, setMenu };
-}
+};
 
-export const useEvents = () => { 
-  const { events } = useContext(UserContext)
+export const useEvents = () => {
+  const { events } = useContext(UserContext);
 
   return events.sort((a, b) => {
     const days = ["Thursday", "Friday", "Saturday", "Sunday"];
@@ -167,17 +182,17 @@ export const useEvents = () => {
       parseTimestamp(a.when) - parseTimestamp(b.when)
     );
   });
-}
+};
 
-export const useTabs = () => { 
+export const useTabs = () => {
   const { activeTab, handleTabClick } = useContext(UserContext);
   return { activeTab, handleTabClick };
-}
+};
 
-export const useDate = () => { 
+export const useDate = () => {
   const { date } = useContext(UserContext);
   return { date };
-}
+};
 
 export const useSearch = () => {
   const { query, setQuery, handleSearch } = useContext(UserContext);
