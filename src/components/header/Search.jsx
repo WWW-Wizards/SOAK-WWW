8eff42fa276c1e50da5d847372e02b38f6a4c459
Dee Larkin <deelarkin@proton.me>
2024-05-16 22:33:49 -0700

    feat: Implements naive string search

---
+++ src/components/header/Search.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { useSearch } from "../../state/StateProvider";
+
+export const Search = () => {
+  const { query, handleSearch } = useSearch();
+
+  return (
+    <div className="search-input-wrapper">
+      <input
+        placeholder="Search..."
+        value={query}
+        onChange={({ target }) => handleSearch(target.value)}
+      />
+    </div>
+  );
+};
