22b5b68d549f37d69aab7f1c724d7d0b34d64dcd
Dee Larkin <deelarkin@proton.me>
2024-05-17 08:29:06 -0700

    feat: Swaps menu icon with SVG from design

--- src/components/header/Menu.jsx
+++ src/components/header/Menu.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useMenu, useFilter, useDate } from "../../state/StateProvider";
 import Saved from "../details/icons/Saved";
 import { Search } from "./Search";
+import MenuIcon from "../details/icons/MenuIcon";
 
 export const Menu = () => {
   const { menu, setMenu } = useMenu();
@@ -11,12 +12,7 @@ export const Menu = () => {
   return (
     <div className="menu-accordion-wrapper">
       <div className="menu-accordion-content" onClick={() => setMenu(!menu)}>
-        <svg
-          className={`menu-accordion-svg ${menu ? "expanded" : ""}`}
-          viewBox="0 0 24 24"
-        >
-          <path d="M16.59 8.59 12 13.17 7.41 8.59 6 10l6 6 6-6z"></path>
-        </svg>
+        <MenuIcon className={`menu-accordion-svg ${menu ? "expanded" : ""}`} />
         <div className="menu-accordion-heading">
           {headingText.toUpperCase()}
         </div>
