9a828bbaef9f550d0b43343aa08dfdf00c172572
Brian O'Neill <boneill3@gmail.com>
2024-05-18 17:47:04 -0700

    Add service worker to index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import { StateProvider } from './state/StateProvider';
 import App from "./App";
 
 console.log("index.js")
+navigator.serviceWorker.register(
+  new URL('service-worker.js', import.meta.url),
+  {type: 'module'}
+);
 const container = document.getElementById("app");
 const root = createRoot(container)
 root.render(
