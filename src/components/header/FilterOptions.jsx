30b56eda7c02b354b68b85a72bf0530303c4d0d2
NickDayFSD <Nick.Day.FSD@gmail.com>
2025-04-11 14:20:46 -0700

    Filter all day Filter switch styling

--- src/components/header/FilterOptions.jsx
+++ src/components/header/FilterOptions.jsx
@@ -8,27 +8,32 @@ function FilterOptions() {
     <div className="menu-filter-container">
       <label htmlFor="pastEvents" className="menu-filter-label">
         Hide Past Events
-        <input
-          type="checkbox"
-          id="pastEvents"
-          name="pastEvents"
-          className="menu-filter-checkbox"
-          value="filterPast"
-          checked={filterPast}
-          onChange={() => setFilterPast(!filterPast)}
-        ></input>
+        <div className="menu-filter-switch">
+          <input
+            type="checkbox"
+            id="pastEvents"
+            name="pastEvents"
+            value="filterPast"
+            checked={filterPast}
+            onChange={() => setFilterPast(!filterPast)}
+          ></input>
+          <span className="menu-filter-slider"></span>
+        </div>
       </label>
       <label htmlFor="allDay" className="menu-filter-label">
         Hide All Day Events
-        <input
-          type="checkbox"
-          id="allDay"
-          name="allDay"
-          className="menu-filter-checkbox"
-          value="filterAllDay"
-          checked={filterAllDay}
-          onChange={() => setFilterAllDay(!filterAllDay)}
-        ></input>
+        <div className="menu-filter-switch">
+          <input
+            type="checkbox"
+            id="allDay"
+            name="allDay"
+            // className="menu-filter-slider"
+            value="filterAllDay"
+            checked={filterAllDay}
+            onChange={() => setFilterAllDay(!filterAllDay)}
+          ></input>
+          <span className="menu-filter-slider"></span>
+        </div>
       </label>
     </div>
   );
