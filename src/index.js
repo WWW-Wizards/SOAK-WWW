0dc86a4bf24b5269dcf9afdd997ece24080b62ff
Dee Larkin <45681154+notnotdee@users.noreply.github.com>
2025-04-30 13:19:36 -0700

    feat: Instrument NR browser agent

--- src/index.js
+++ src/index.js
@@ -1,14 +1,41 @@
+// Init NR browser agent
+import { BrowserAgent } from "@newrelic/browser-agent";
+const options = {
+  init: {
+    privacy: { cookies_enabled: true },
+    page_view_timing: { enabled: true },
+    spa: { enabled: true },
+    ajax: { deny_list: ["bam.nr-data.net"] },
+    distributed_tracing: { enabled: false },
+  },
+  info: {
+    beacon: "bam.nr-data.net",
+    errorBeacon: "bam.nr-data.net",
+    licenseKey: "NRJS-951885a3654f8493a1b",
+    applicationID: "1134551812",
+    sa: 1,
+  },
+  loader_config: {
+    accountID: "3241619",
+    trustKey: "3241619",
+    agentID: "1134551812",
+    licenseKey: "NRJS-951885a3654f8493a1b",
+    applicationID: "1134551812",
+  },
+};
+new BrowserAgent(options);
+
 import { createRoot } from "react-dom/client";
-import { StateProvider } from './state/StateProvider';
+import { StateProvider } from "./state/StateProvider";
 import App from "./App";
 
-console.log("index.js")
 navigator.serviceWorker.register(
-  new URL('service-worker.js', import.meta.url),
-  {type: 'module'}
+  new URL("service-worker.js", import.meta.url),
+  { type: "module" }
 );
+
 const container = document.getElementById("app");
-const root = createRoot(container)
+const root = createRoot(container);
 root.render(
   <StateProvider>
     <App />
