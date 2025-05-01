fc16525d4d96b6464ed7527fbab8385ed8b3abb6
Brian O'Neill <boneill3@gmail.com>
2025-05-01 07:39:59 -0700

    Fix mock service worker for gh pages

--- src/index.js
+++ src/index.js
@@ -45,7 +45,11 @@ async function enableMocking() {
 
   // `worker.start()` returns a Promise that resolves
   // once the Service Worker is up and ready to intercept requests.
-  return worker.start();
+  return worker.start({
+    serviceWorker: {
+      url: './mockServiceWorker.js',
+    },
+  })
 }
 const container = document.getElementById("app");
 const root = createRoot(container);
